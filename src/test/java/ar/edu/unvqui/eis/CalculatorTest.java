b557c0e0b7ef681fa04a152dda287bbaa4c66f09
Diego <diego.sanches@gmail.com>
2019-09-20 19:05:44 +0000

    Initial commit

---
+++ src/test/java/ar/edu/unvqui/eis/CalculatorTest.java
@@ -0,0 +1,22 @@
+package ar.edu.unvqui.eis;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAddBothNumber()
+    {
+
+        assertEquals(4, Calculator.add(2,2));
+    }
+}
